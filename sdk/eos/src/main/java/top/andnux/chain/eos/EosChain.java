15dc2238fce93d17a3e25236ba53a5eaba942d5a
zhangchunlin <zhangchunlin@fishotech.com>
2019-12-17 14:25:00 +0800

    重构链 支持创建账号

--- sdk/eos/src/main/java/top/andnux/chain/eos/EosChain.java
+++ sdk/eos/src/main/java/top/andnux/chain/eos/EosChain.java
@@ -7,8 +7,4 @@ import top.andnux.chain.core.Provider;
 @Provider(EosChainImpl.class)
 public interface EosChain extends Chain<EosAccount, EosTransferParams> {
 
-    EosAccount createAccount() throws Exception;
-
-    EosAccount createAccountByPrivateKey(String privateKey) throws Exception;
-
 }
